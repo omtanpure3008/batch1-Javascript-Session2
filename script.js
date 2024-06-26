859fddc97655e04c4c5bad97585f669e6dfb499b
omtanpure3008 <162991862+omtanpure3008@users.noreply.github.com>
2024-06-26 08:44:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const heading = document.querySelector('.wrapper h1');
+console.log(heading);
+
+const btn = document.querySelector('.wrapper button');
+console.log(btn);
+
+const wrapper = document.querySelector('.wrapper');
+console.log(wrapper);
+
+// heading.style.background = "linear-gradient(to right, purple, red)";
+// heading.style.WebkitBackgroundClip = "text";
+// heading.style.WebkitTextFillColor = "transparent";
+// heading.style.fontSize = "2vw";
+
+
+// heading.setAttribute('id', 'abc');
+
+// heading.removeAttribute('id');
+
+// console.log(heading.getAttribute('id'));
+
+
+
+
+// heading.classList.add('abc');
+
+// heading.classList.remove('abc');
